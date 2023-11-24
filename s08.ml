2ec4c0b24a7f23dbd1f0697a1d403fa6eaeb50b8
Christian <1304096+cfax@users.noreply.github.com>
2023-11-24 21:53:44 +0000

    Reduce duplicate cases

--- s08.ml
+++ s08.ml
@@ -6,9 +6,8 @@
 
 let rec compress (xs: 'a list): ('a list) =
     match xs with
-    | [] -> []
-    | [x] -> [x]
     | x :: y :: xs -> if x = y
         then compress (x :: xs)
         else x :: (compress (y :: xs))
+    | _ -> xs
 
