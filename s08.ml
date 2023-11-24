2198e9a4a8d0945bd1aa569f9bd10567efb67ff9
Christian <1304096+cfax@users.noreply.github.com>
2023-11-24 21:52:02 +0000

    Add solution for problem 8 (first iteration)

---
+++ s08.ml
@@ -0,0 +1,14 @@
+(* Eliminate consecutive duplicates of list elements. (medium)
+
+# compress ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"];;
+- : string list = ["a"; "b"; "c"; "a"; "d"; "e"]
+*)
+
+let rec compress (xs: 'a list): ('a list) =
+    match xs with
+    | [] -> []
+    | [x] -> [x]
+    | x :: y :: xs -> if x = y
+        then compress (x :: xs)
+        else x :: (compress (y :: xs))
+
