dbc26a5596cdb7ce2bf7e9bc262b5ae589a10c77
Christian <1304096+cfax@users.noreply.github.com>
2023-11-24 21:33:06 +0000

    Add solution for problem 4 (TCO)

--- s04.ml
+++ s04.ml
@@ -13,3 +13,10 @@ let rec length (xs: 'a list): int =
     match xs with
     | [] -> 0
     | x :: xs -> 1 + length xs
+
+let length' (xs: 'a list): int =
+    let rec length_tco (xs: 'a list) (acc: int): int =
+        match xs with
+        | [] -> acc
+        | _ :: xs -> length_tco xs (acc + 1)
+    in length_tco xs 0
